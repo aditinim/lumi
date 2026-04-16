09817c3fdcf24486bdb172cb6fbecb6133638702
Aditi Nim <nimaditi7@gmail.com>
2026-04-16 14:45:18 +0530

    blacklisting

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,14 @@
 const express= require("express");
 const authController= require("../controller/auth.controller");
+const authMiddleware = require("../middleware/auth.middleware");
 
 const router= express.Router();
 
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
+router.get("/get-me", authMiddleware.authUser, authController.getMe);
+router.get("/logout", authController.logoutUser);
+
 
 
 module.exports= router;
